494259c017ae34be5fa8731ab6cef053298bcd53
TonyPepe <41405045+TonyPepeBear@users.noreply.github.com>
2023-04-25 13:57:05 +0800

    unocss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script>
 	import data from '../data/data.json';
+    import 'virtual:uno.css'
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<p>Name: {data.name}</p>
+<p class="text-4xl">Name: {data.name}</p>
 <p>Age: {data.age}</p>
 <p>City: {data.city}</p>
 
