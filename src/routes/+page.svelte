b983e5254f83cefc866f546595a30b653f80c05a
TonyPepe <41405045+TonyPepeBear@users.noreply.github.com>
2023-04-25 12:56:34 +0800

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+	import data from '../data/data.json';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>Name: {data.name}</p>
+<p>Age: {data.age}</p>
+<p>City: {data.city}</p>
+
