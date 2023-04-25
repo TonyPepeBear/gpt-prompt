f4c06710f2e6768daf3328843f662f4293efed14
TonyPepe <tonybear55665566@gmail.com>
2023-04-25 20:12:05 +0800

    Add virtual:uno.css and Navbar component; update app.html with margin: 0 on body tag

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,7 @@
+<script>
+    import 'virtual:uno.css'
+</script>
+
+<div class="w-full h-10 bg-green">
+    
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
 	import data from '../data/data.json';
     import 'virtual:uno.css'
+	import Navbar from '$lib/components/navbar.svelte';
 </script>
+
+<Navbar />
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
@@ -9,4 +12,3 @@
 <p class="text-4xl">Name: {data.name}</p>
 <p>Age: {data.age}</p>
 <p>City: {data.city}</p>
-
